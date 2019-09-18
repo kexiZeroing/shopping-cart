12aa5e255c413a24c8119b6b54fb4f1f59264464
dangkexi <dangkexi@gmail.com>
2019-09-18 15:37:07 -0500

    styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 import logo from "../logo.svg";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -17,12 +19,40 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
+          <ButtonContainer>
             <span className="mr-2"><i className="fas fa-cart-plus" /></span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </Nav>
     )
   }
 }
+
+const Nav = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+`;
+
+// styled-components
+// const ButtonContainer = styled.button`
+//   text-transform: capitalize;
+//   font-size: 1.4rem;
+//   background: transparent;
+//   border: 0.05rem solid var(--lightBlue);
+//   border-radius: 0.5rem;
+//   padding: 0.2rem 0.5rem;
+//   outline-color: red;
+//   cursor: pointer;
+//   display: inline-block;
+//   margin: 0.2rem 0.5rem 0.2rem 0;
+//   transition: all 0.5s ease-in-out;
+//   &:hover {
+//     background: var(--lightBlue);
+//     color: var(--mainBlue);
+//   }
+// `;
